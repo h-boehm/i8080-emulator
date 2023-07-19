c01271df37519b766863f8b7df50887a766627aa
Christopher Tu <tuchr@oregonstate.edu>
2023-07-18 22:09:39 -0700

    combining the memory and cpu modules

--- main.c
+++ main.c
@@ -16,24 +16,22 @@ int main(int argc, char **argv)
     // when running the executable
     print_memory();
 
-    /*
-        // set up a State8080 struct
-        State8080 cpu_state;
-        // int pc = 0;
-        //  try setting the initial pc value
-        cpu_state.pc = 0;
-        cpu_state.memory = buffer;
+    // set up a State8080 struct
+    State8080 cpu_state;
+    // int pc = 0;
+    //  try setting the initial pc value - should point to the start of the program
+    cpu_state.pc = 0;
+    cpu_state.memory = memory;
 
-        while (cpu_state.pc < fsize)
-        {
-            // pc += Disassemble8080Op(buffer, pc);
-            // cpu_state.pc += Disassemble8080Op(buffer, cpu_state.pc);
-            Emulate8080Op(&cpu_state);
+    while (cpu_state.pc < sizeof(memory))
+    {
+        // pc += Disassemble8080Op(buffer, pc);
+        // cpu_state.pc += Disassemble8080Op(buffer, cpu_state.pc);
+        Emulate8080Op(&cpu_state);
 
-            // wait for user to press enter before going to next instruction
-            // getchar();
-        }
+        // wait for user to press enter before going to next instruction
+        // getchar();
+    }
 
-    */
     return 0;
 }
--- modules/memory.c
+++ modules/memory.c
@@ -10,10 +10,11 @@
 // instructions/operands are unsigned chars
 unsigned char memory[MEM_SIZE];
 
-void load_file(char *file, int address) 
+void load_file(char *file, int address)
 {
     FILE *f = fopen(file, "rb");
-    if (f == NULL) {
+    if (f == NULL)
+    {
         printf("error: Couldn't open %s\n", file);
         exit(1);
     }
@@ -27,25 +28,28 @@ void load_file(char *file, int address)
     fclose(f);
 }
 
-unsigned char mem_read(int address) 
+unsigned char mem_read(int address)
 {
     // read one byte from memory
     return memory[address];
 }
 
-void mem_write(int address, unsigned char byte) 
+void mem_write(int address, unsigned char byte)
 {
     // write one byte to memory
     // first 2K bytes are read-only memory
-    if (address < 0x800) {
+    if (address < 0x800)
+    {
         printf("error: Can't write to ROM at address %x\n", address);
-    } else {
+    }
+    else
+    {
         // set address to byte
         memory[address] = byte;
     }
 }
 
-void mem_init() 
+void mem_init()
 {
     // initialize memory
     // clear memory buffer (set all bytes to 0)
@@ -53,20 +57,22 @@ void mem_init()
 
     // load game files (each one is 2048 (or 0x800) bytes)
     // start from RAM address
-    load_file("./invaders/invaders.e", 0x800);
+    load_file("./invaders/invaders.h", 0x0000);
+    load_file("./invaders/invaders.g", 0x0800);
     load_file("./invaders/invaders.f", 0x1000);
-    load_file("./invaders/invaders.g", 0x1800);
-    load_file("./invaders/invaders.h", 0x2000);
+    load_file("./invaders/invaders.e", 0x1800);
 }
 
-void print_memory() 
+void print_memory()
 {
     // starting from RAM
-    for (int i = 0; i < 16383; ++i) {
+    for (int i = 0; i < 16383; ++i)
+    {
         // pad hex values for clearer output
         printf("%02x ", memory[i]);
         // newline every 16 bytes
-        if ((i + 1) % 16 == 0) {
+        if ((i + 1) % 16 == 0)
+        {
             printf("\n");
         }
     }
