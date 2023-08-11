0993d3a7054189bf2aad2c7752837a99bbd80801
Christopher Tu <tuchr@oregonstate.edu>
2023-08-11 14:33:18 -0700

    remove unused sound struct

--- include/sounds.h
+++ include/sounds.h
@@ -5,14 +5,6 @@
 #include <SDL2_mixer/SDL_mixer.h>
 #include "controls.h"
 
-typedef struct Sound
-{
-    char *file_path;
-    SDL_AudioSpec wav_spec;
-    uint32_t wav_length;
-    uint8_t *wav_buffer;
-} Sound;
-
 void init_sounds();
 void play_sounds(SpaceInvadersMachine *machine);
 
